fefb5ae2ea97f169de3fcdaef6fb1d78af125369
ALUMNADO\healgo <healgo@floridauniversitaria.es>
2023-02-07 12:38:42 +0100

    commit1

---
+++ test/enanaTest.php
@@ -0,0 +1,51 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+include './src/Enana.php';
+
+class EnanaTest extends TestCase {
+
+    public function testHeridaLeveVive() {
+       $enana= new Enana('nombre', 30, 'viva');
+       $enana->heridaLeve();
+        #Se probará el efecto de una herida leve a una Enana con puntos de vida suficientes para sobrevivir al ataque
+        #Se tendrá que probar que la vida es mayor que 0 y además que su situación es viva
+
+    }
+
+    public function testHeridaLeveMuere() {
+        $enana= new Enana('nombre', 5, 'viva');
+        $enana->heridaLeve();
+        #Se probará el efecto de una herida leve a una Enana con puntos de vida insuficientes para sobrevivir al ataque
+        #Se tendrá que probar que la vida es menor que 0 y además que su situación es muerta
+
+    }
+
+    public function testHeridaGrave() {
+        $enana= new Enana('nombre', 30, 'viva');
+       $enana->heridaGrave();
+       
+        #Se probará el efecto de una herida grave a una Enana con una situación de viva.
+        #Se tendrá que probar que la vida es 0 y además que su situación es limbo
+
+    }
+    
+    public function testPocimaRevive() {
+        $enana= new Enana('nombre', 20, 'viva');
+        $enana->pocima();
+        #Se probará el efecto de administrar una pócima a una Enana muerta pero con una vida mayor que -10 y menor que 0
+        #Se tendrá que probar que la vida es mayor que 0 y que su situación ha cambiado a viva
+
+    }
+
+    public function testPocimaExtraLimbo() {
+        $enana= new Enana('nombre', 0, 'limbo');
+        $enana->pocimaExtra();
+        #Se probará el efecto de administrar una pócima Extra a una Enana en el limbo.
+        #Se tendrá que probar que la vida es 50 y la situación ha cambiado a viva.
+
+    }
+}
+
+
+?>
